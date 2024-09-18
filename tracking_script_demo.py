5b32f5e3cec253f093f235eee483cb17289918d9
broczniok <broczniok@griddynamics.com>
2024-09-18 10:07:13 +0200

    changed insertion and deletion

--- tracking_script_demo.py
+++ tracking_script_demo.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, max
+from pyspark.sql.functions import col, max, row_number, lit, to_timestamp
+from pyspark.sql import Window
 from datetime import datetime
 
 spark = SparkSession.builder \
@@ -7,90 +8,67 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 END_TIME = '9999-12-31 23:59:59'
-
-DIM_ID = 1
-if spark.read.table("demo.iceberg.mobileapp").agg(max("dim_id")).collect()[0][0] is not None:
-    DIM_ID = spark.read.table("demo.iceberg.mobileapp").agg(max("dim_id")).collect()[0][0] + 1
+dim_df = spark.read.table("demo.iceberg.mobileapp")
+current_max_dim_id = dim_df.agg(max("dim_id")).collect()[0][0] or 0
 
 
-def check_deletion(source_row):
-    dim_df = spark.read.table("demo.iceberg.mobileapp")
+def check_deletion(source_df):
+    dim_df_selected = dim_df.select("dim_id", "userId", "eventId", "eventType", "eventTime", "attributes", "action_cd", "created_ts", "expired_ts")
+    source_df_selected = source_df.select("userId", "eventId", "eventType", "eventTime", "attributes", "date")
 
-    source_id = source_row['userId']
+    left_outer_join_df = dim_df_selected.alias("dim") \
+        .join(source_df_selected.alias("src"),
+              (col("dim.userId") == col("src.userId")) &
+              (col("dim.eventId") == col("src.eventId")),
+              "left_outer")
 
-    if dim_df.filter(col("userId") == source_id).count() > 0:
-        spark.sql(f"""
-            UPDATE demo.iceberg.mobileapp 
-            SET action_cd = 'D', expired_ts = '{str(datetime.now())}' 
-            WHERE userId = '{source_id}'
-        """)
+    deleted_data_df = left_outer_join_df.filter(col("src.userId").isNull()).select("dim.*")
+    deleted_data_df = deleted_data_df.withColumn("action_cd", lit('D')
+                                                 ).withColumn("expired_ts", to_timestamp(lit(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))))
 
-        spark.sql(f"""
-            INSERT INTO demo.iceberg.mobileapp 
-            VALUES ({DIM_ID}, '{source_id}', 
-                    '{source_row['eventId']}', 
-                    '{source_row['eventType']}', 
-                    '{source_row['eventTime']}', 
-                    '{str(source_row['attributes']).replace("'", "''")}', 
-                    'D', '{str(datetime.now())}', '{END_TIME}')
-        """)
-        print(f"Deleted user: {source_id}")
+    deleted_data_df.select(
+        "dim_id", "userId", "eventId", "eventType", "eventTime", "attributes", "action_cd", "created_ts", "expired_ts"
+    ).write.insertInto("demo.iceberg.mobileapp", overwrite=True)
 
 
-def check_insertion(source_row):
-    dim_df = spark.read.table("demo.iceberg.mobileapp")
+def check_insertion(source_df):
 
-    source_id = source_row['userId']
+    joined_df = source_df.alias('src') \
+        .join(dim_df.alias('dim'), col('src.userId') == col('dim.userId'), 'left') \
+        .select('dim.dim_id','src.*','dim.action_cd','dim.created_ts','dim.expired_ts')
 
-    if dim_df.filter(col("userId") == source_id).count() == 0:
+    new_record_df = joined_df.filter(col("dim_id").isNull())
 
-        spark.sql(f"""
-            INSERT INTO demo.iceberg.mobileapp 
-            VALUES ({DIM_ID}, '{source_id}', 
-                    '{source_row['eventId']}', 
-                    '{source_row['eventType']}', 
-                    '{source_row['eventTime']}', 
-                    '{str(source_row['attributes']).replace("'", "''")}', 
-                    'I', '{str(datetime.now())}', '{END_TIME}')
-        """)
-        print(f"Inserted user: {source_id}")
-
-
-def check_update(source_row):
-    dim_df = spark.read.table("demo.iceberg.mobileapp")
-
-    source_id = source_row['userId']
-
-    if dim_df.filter(col("userId") == source_id).count() > 0:
-        existing_record = dim_df.filter(col("userId") == source_id).first()
-
-        fields_to_compare = ['eventId', 'eventType', 'eventTime', 'attributes']
-        has_changes = any(
-            source_row[field] != getattr(existing_record, field) for field in fields_to_compare
+    if new_record_df.count() > 0:
+        window_spec = Window.orderBy("userId")
+        new_records_with_dim_id = new_record_df.withColumn(
+            "dim_id", row_number().over(window_spec) + lit(current_max_dim_id)
+        ).withColumn(
+            "action_cd", lit('I')
+        ).withColumn(
+            "created_ts", to_timestamp(lit(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        ).withColumn(
+            "expired_ts", to_timestamp(lit(END_TIME))
         )
 
-        if has_changes:
-            spark.sql(f"""
-                INSERT INTO demo.iceberg.mobileapp 
-                VALUES ({DIM_ID}, '{source_row['userId']}', 
-                        '{source_row['eventId']}', 
-                        '{source_row['eventType']}', 
-                        '{source_row['eventTime']}', 
-                        '{str(source_row['attributes']).replace("'", "''")}', 
-                        'U', '{str(datetime.now())}', '{END_TIME}')
-            """)
-            print(f"Updated user: {source_id}")
+        new_records_with_dim_id.select(
+            "dim_id", "userId", "eventId", "eventType", "eventTime", "attributes", "action_cd", "created_ts", "expired_ts"
+        ).write.insertInto("demo.iceberg.mobileapp", overwrite=False)
+
+
+def check_update(source_df):
+    ...
+
 
 def read_parquets(file_path):
     df = spark.read.parquet(file_path)
 
-    for row in df.collect():
-        check_insertion(row)
-        check_update(row)
-        check_deletion(row)
+    check_insertion(df)
+    #check_update(df)
+    check_deletion(df)
 
-    spark.stop()
 
 if __name__ == "__main__":
     read_parquets("parquets")
-    spark.table("demo.iceberg.mobileapp").show()
+    spark.stop()
+
